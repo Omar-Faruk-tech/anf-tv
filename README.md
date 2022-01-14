591b1f8dbccceff572e9822dc2de4cdad7a2d38d
unknown <farukadekunle@gmail.com>
2022-01-14 17:24:52 +0100

    nav animation

---
+++ README.md
@@ -0,0 +1,6 @@
+ANF-tv is a website where you can get access to latest news content and videos. 
+
+Contents will be uploaded by the site admin. Each upload will will carry all needed information like the news category, image/video, subject/title and content.
+The site don't store user's information, anyone who visits the site can have access to view all uploaded contents.
+
+This project's design and implementation is still ongoing.
--- js/index.js
+++ js/index.js
@@ -2,6 +2,17 @@
 let newsButtonContainer = 
 document.getElementById('news-list-container');
 
+let modal = document.getElementsByClassName('modal-backgroung');
+let modalId = document.getElementById('modal-backgroung');
+
 function toggleNav() {
-  newsButtonContainer.classList.toggle('open-news-categories')
+  if (modal[0] == null) {
+    // console.log('hello')
+    newsButtonContainer.classList.add('open-news-categories');
+    modalId.classList.add('modal-backgroung');
+  } else {
+    newsButtonContainer.classList.remove('open-news-categories');
+    // newsButtonContainer.classList.toggle('close-news-categories');
+    modalId.classList.remove('modal-backgroung');
+  }
 };
